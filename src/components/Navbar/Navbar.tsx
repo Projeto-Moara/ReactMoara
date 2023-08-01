1ca0c911e3c4ed520a8ee234207b5a0bbc54c740
Gabriela Guerra <gabrielaguerra438@gmail.com>
2023-08-01 11:48:15 -0300

    @GWarriorF @RitaAlmeidah @Maysouzac @Kauanhassani @EmilySantoos @ElsonSilv4 lógica de login, cadastro e autenticação do usuário

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
+
 
 function Navbar() {
-
+ 
+  
 
   return (
     <>
-     <div className='w-full bg-[#e8ee70] text-white flex justify-center py-4'>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase text-[#024D78]'>Moara</div>
+            <div className='text-2xl font-bold uppercase'>Moara</div>
 
             <div className='flex gap-4'>
-              <div className='hover:underline text-[#024D78]'>
-                <Link to='/home' className='hover:underline'>Home</Link>
-              </div>
-              <div className='hover:underline text-[#024D78]'>
-                <Link to='/login' className='hover:underline'>Login</Link>
-              </div>
-              <div className='hover:underline text-[#024D78]'>
+          
+              <div className='hover:underline'>Postagens</div>
+              <div className='hover:underline'>Temas</div>
+              <div className='hover:underline'>Cadastrar tema</div>
+              <div className='hover:underline'>
                 <Link to='/sobre' className='hover:underline'>Sobre</Link>
               </div>
-              <div className='hover:underline text-[#024D78]'>Postagens</div>
-              <div className='hover:underline text-[#024D78]'>Temas</div>
-              <div className='hover:underline text-[#024D78]'>Cadastrar tema</div>
-              <div className='hover:underline text-[#024D78]'>Perfil</div>
-              <div className='hover:underline text-[#024D78]'>Sair</div>
+              <div className='hover:underline'>Perfil</div>
+              <div className='hover:underline'>Sair</div>
+             
             </div>
           </div>
         </div>
