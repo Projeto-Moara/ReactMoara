ac94052d4d87e6930182c6c23b900f2f6383cd60
rita_@Almeida <rita_aja92@hotmail.com>
2023-08-02 18:42:27 -0300

    @RitaAlmeidah - Correção navBar no Link de Home e Link de Sair

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
-import { Link, useNavigate } from 'react-router-dom'
-import React from 'react'
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 
 
 function Navbar() {
  
-  
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
+ 
 
   return (
     <>
@@ -13,15 +25,25 @@ function Navbar() {
             <div className='text-2xl font-bold uppercase'>Moara</div>
 
             <div className='flex gap-4'>
-          
+              <div className='hover:underline'>
+              <Link to='/home' className='hover:underline'>Home</Link>
+              </div>
+
               <div className='hover:underline'>Postagens</div>
+
               <div className='hover:underline'>Temas</div>
+              
               <div className='hover:underline'>Cadastrar tema</div>
+
               <div className='hover:underline'>
                 <Link to='/sobre' className='hover:underline'>Sobre</Link>
               </div>
+
               <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+
+              <div className='hover:underline'>
+              <Link to='' onClick={logout} className='hover:underline m-4'>Sair</Link>
+              </div>
              
             </div>
           </div>
