2b3202404e73531ef7be755a4613d57d58bae33a
Gabriela Guerra <gabrielaguerra438@gmail.com>
2023-08-22 23:49:46 -0300

    @GWarriorF Integração da aplicação completa

--- src/components/temas/cardTemas/CardTemas.tsx
+++ src/components/temas/cardTemas/CardTemas.tsx
@@ -6,13 +6,7 @@ interface CardTemaProps {
 
 function CardTemas({ tema }: CardTemaProps) {
   return (
-<<<<<<< HEAD
-    <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-      <header className='py-2 px-6 bg-gradient-to-r from-[#02123E] via-[#06315E] to-[#02123E] rounded-lg drop-shadow-xl text-[white] font-bold text-2xl'>Tema</header>
-      <p className='p-8 text-3xl bg-slate-200 h-full'>{tema?.nome}</p>
-      <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
-      <div className="flex">
-=======
+
 
     <div className="flex bg-[#121D13] displayCounteiner mt-[4%] items-center justify-center w-[65vw] h-[175px]">
 
@@ -28,7 +22,6 @@ function CardTemas({ tema }: CardTemaProps) {
 
 
       {/* <div className="flex">
->>>>>>> eec3db7543fc5ed11972ba3086ddb39d5990e079
         <Link to={`/editarTema/${tema.id}`} className='w-full text-slate-100 bg-gradient-to-r from-green-500 to-blue-600   hover:from-blue-400 hover:to-green-400 flex items-center justify-center py-2'>
           <button>Editar</button>
         </Link>
